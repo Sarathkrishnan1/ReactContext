5e1d537b4dbaa5a0943381b475a40e908dd8ead9
Sarath krishnan <82701589+Sarathkrishnan1@users.noreply.github.com>
2021-05-06 18:01:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+
+import { GlobalProvider } from './context/GlobalState';
+
+import { Home } from './components/Home';
+import { AddEmployee } from './components/AddEmployee';
+import { EditEmployee } from './components/EditEmployee';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <div className="App">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/add" component={AddEmployee} exact />
+          <Route path="/edit/:id" component={EditEmployee} exact />
+        </Switch>
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
